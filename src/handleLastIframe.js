0e9d65ea5ee5ddf7456c38a428aa27222798c58f
John Livingston <jmlivingston@gmail.com>
2021-09-01 10:45:33 -0700

    Tweak handleLastIframe

--- src/handleLastIframe.js
+++ src/handleLastIframe.js
@@ -1,8 +1,10 @@
 // Some sites inject iframes randomly that cover the entire page
 function handleLastIframe() {
-  const lastIframe = Array.from(document.getElementsByTagName('iframe')).pop()
-  lastIframe.style.height = 0;
-  lastIframe.style.width = 0;
+  const lastIframe = Array.from(document.getElementsByTagName('iframe'))?.pop();
+  if (lastIframe) {
+    lastIframe.style.height = 0;
+    lastIframe.style.width = 0;
+  }
 }
 
-handleLastIframe()
+handleLastIframe();
